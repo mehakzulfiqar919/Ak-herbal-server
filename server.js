1cc8edddf7ca5bc9e03a68524f63a6c845ab0855
mehakzulfiqar919 <mehakzulfiqar919@gmail.com>
2026-07-01 01:56:31 +0500

    fixing privacy

--- server.js
+++ server.js
@@ -12,13 +12,13 @@ const orderRoutes = require("./routes/orderRoutes");
 connectDB();
 
 const app = express();
-
 app.use(
   cors({
-    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    origin: "*",
     credentials: true,
   })
 );
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
