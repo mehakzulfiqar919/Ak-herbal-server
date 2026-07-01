0365a86c57ab227bf51952aa668314780ef1b1f8
mehakzulfiqar919 <mehakzulfiqar919@gmail.com>
2026-07-02 01:45:18 +0500

    last chngee

--- server.js
+++ server.js
@@ -12,7 +12,12 @@ const orderRoutes = require("./routes/orderRoutes");
 connectDB();
 
 const app = express();
-app.use(cors());
+app.use(
+  cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials: true,
+  })
+);
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
